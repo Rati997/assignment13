ce5bd99fcbbeffc2add803c76a6191b575d4b6c5
rati <rmekokishvili3gmail.com>
2023-08-06 20:21:45 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,83 @@
+ //   1 davaleba
+
+
+
+        //    let number = prompt ('enter a number')         
+        //     let result = 0;
+        //   for(let i = 0; i < number.length; i++) {
+        //     result += +number[i];
+        //   }
+        //   alert(result);
+
+
+
+        // 2 davaleba
+
+        // let number1 = prompt('enter min');
+        // let number2 = prompt('enter max');
+
+        // let int1 = +number1;
+        // let int2 = +number2;
+
+        // let result = "";
+        // for (let i = int1; i <= int2; i++) {
+        //     if (i % 2 === 0) {
+        //         result += i + " ";
+        //     }
+        // }
+
+        // alert(result);
+
+
+        // 2davaleba+
+
+        // let number1 = 5;
+        // let number2 = 14;
+
+        // let int1 = +number1;
+        // let int2 = +number2;
+
+        // let result = "";
+        // for (let i = int1; i <= int2; i++) {
+        //     if (i % 2 === 0) {
+        //         result += i + " ";
+        //     }
+        // }
+
+        // alert(result);
+
+
+
+
+        // 3davaleba
+
+
+        // let num = prompt('num');
+
+        // num = +num;
+        // oneRow = "";
+        // result = "";
+
+        // let terminationString = "\n"
+        // for (let i = 0; i < num; i++) {
+        //     oneRow += "*";
+        // }
+
+        // oneRow += terminationString;
+
+        // for (let i = 0; i < num; i++) {
+        //     result += oneRow;
+        // }
+
+        // alert(result);
+
+
+        // 4davaleba
+
+
+        // let number1 = prompt('enter number1');
+        // let number2 = prompt('enter number2');
+        
+        // alert(Math.pow(number1, number2));
+
+       
